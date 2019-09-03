b5c8ada7aa0d727cc27439aebbd36c4820892482
lavy0228 <54863515+lavy0228@users.noreply.github.com>
2019-09-03 14:19:56 -0500

    Create gitGetUsrInfo.java

---
+++ gitGetUsrInfo.java
@@ -0,0 +1,14 @@
+import org.springframework.web.client.RestTemplate;	
+
+public class GetGitHubUserInfo {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        GitHubUser user = restTemplate.getForObject("https://api.github.com/users/lavy0228", GitHubUser.class);
+        
+        System.out.println("Login   :    " + user.getLogin());
+        System.out.println("Email   :    " + user.getEmail());
+        System.out.println("Company :    " + user.getCompany());
+        System.out.println("Location:    " + user.getLocation());
+        System.out.println("Created :    " + user.getCreated_at());
+    }
+}
